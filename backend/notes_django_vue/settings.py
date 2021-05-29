6eaeef951bb1915f9f5d2ba911040999e0315a4c
Boris Zakaraya <zakaraya2000@mail.ru>
2021-05-29 17:53:40 +0300

    finish

--- backend/notes_django_vue/settings.py
+++ backend/notes_django_vue/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'notes',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'notes_django_vue.urls'
 
